db88008736c371dd5cbbc04e47f286066ba0b1d7
Artem Laptiev <arlaptiev@gmail.com>
2022-12-11 03:08:32 -0500

    player+selector

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,34 @@
+import { useEffect } from 'react';
+import Player from '../utils/player';
+import Selector from '../utils/selector';
+
 function App() {
+  const tracks = [
+    {filename: '130_Cm_Melodic_SPFT_126_05.wav'},
+    {filename: '130_Synth_Loop_15_134_SP.wav'}
+  ]
+  const options = {
+    bpm: 130,
+    // barLength: 4000,
+  }
+
+  const player = new Player(tracks, options)
+  const selector = new Selector(player)
+  
   return (
     <div class="container m-auto">
-      <p class="text-center text-6xl font-bold mt-44"> Quick App </p>
+      <p class="text-center text-6xl font-bold mt-44"> BodyBeat </p>
+      <div class="m-auto">
+        <button class="bg-orange-200 p-2" onClick={() => player.startAll()}>START1</button>
+        <button class="bg-orange-200 p-2" onClick={() => selector.init()}>START2</button>
+        <button class="bg-orange-200 p-2" onClick={() => selector.process(2)}>ADDENERGY</button>
+        <button class="bg-orange-200 p-2" onClick={() => selector.process(0)}>REMOVEENERGY</button>
+        {/* <button class="bg-orange-200 p-2" onClick={() => player.startOnBar(tracks[0].filename)}>QUEUE1</button>
+        <button class="bg-orange-200 p-2" onClick={() => player.startOnBar(tracks[1].filename)}>QUEUE2</button>
+        <button class="bg-orange-200 p-2" onClick={() => player.startAll()}>STARTALL</button>
+        <button class="bg-orange-200 p-2" onClick={() => player.stopAll()}>STOPALL</button>
+        <button class="bg-orange-200 p-2" onClick={() => player.setParamFunction(tracks[0].filename, 'gain', 0, 4)}>CHANGEGAIN1</button> */} */}
+      </div>
     </div>
   );
 }
---
+++ src/utils/selector.js
@@ -0,0 +1,90 @@
+const MIN_ENERGY = 0
+const MAX_ENERGY = 4
+const ENERGY_LEVEL_THRESHOLD = {
+  0: 1,
+  1: 2,
+  2: 3,
+  3: 4
+}
+
+class Selector {
+  constructor(player) {
+    this.player = player
+    this.library = {
+      tracks: {
+        0: [{filename: '130_Synth_Loop_15_134_SP.wav'}],
+        1: [{filename: '130_Cm_Melodic_SPFT_126_05.wav'}],
+        2: [],
+        3: [],
+        4: []
+      }
+    }
+    this.composition = {
+      energy: 0,
+      tracks: {
+        0: [],
+        1: [],
+        2: [],
+        3: [],
+        4: []
+      }
+    }
+  }
+
+  init() {
+    this.composition.tracks[0].push({filename: '130_Synth_Loop_15_134_SP.wav'})
+    this.addLoop(this.composition.tracks[0][0])
+  }
+
+  calcEnergy(input) {
+    return input
+  }
+
+  incrementEnergyLevel() {
+    this.composition.energy++
+    const options = this.library.tracks[this.composition.energy]
+    const track = options[Math.floor(Math.random() * options.length)]
+    this.composition.tracks[this.composition.energy].push(track)
+    this.addLoop(track)
+  }
+
+  decrementEnergyLevel() {
+    this.composition.energy--
+    const playingTracks = this.composition.tracks[this.composition.energy + 1]
+    for (let i = playingTracks.length - 1; i >= 0; i -= 1) {
+      this.removeLoop(playingTracks[i])
+      playingTracks.splice(i, 1);
+    }
+  }
+
+  process(input) {
+    // let updates = []
+    // register events
+    // maybe add randomness
+    let inputEnergy = this.calcEnergy(input)
+    if (this.composition.energy < MAX_ENERGY && inputEnergy > ENERGY_LEVEL_THRESHOLD[this.composition.energy]) { 
+      this.incrementEnergyLevel()
+    }
+    if (this.composition.energy > MIN_ENERGY && inputEnergy < ENERGY_LEVEL_THRESHOLD[this.composition.energy - 1]) { 
+      this.decrementEnergyLevel()
+    }
+    // process continuous input
+  }
+
+  addLoop(track) {
+    if (track.filename in this.player.tracks) {
+      this.player.startOnBar(track.filename)
+    } else {
+      this.player.loadTrack(track)
+        .then(() => {
+          this.player.startOnBar(track.filename)
+        })
+    }
+  }
+
+  removeLoop(track) {
+    this.player.stopOnBar(track.filename)
+  }
+}
+
+export default Selector
