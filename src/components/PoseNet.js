6a3daaf7b0e0200afa80e6c73781fe0e00b443df
Tom Cobley <43605582+tomcobley@users.noreply.github.com>
2022-12-11 23:17:53 -0500

    Cleanup

--- src/components/PoseNet.js
+++ src/components/PoseNet.js
@@ -2,25 +2,17 @@ import * as ml5 from "ml5";
 import Sketch from "react-p5";
 import $ from 'jquery';
 import {useEffect, useState} from "react";
-import {analysePoses} from "../utils/poseAnalyser";
 import {drawKeypoints, drawSkeleton} from "../utils/poseVisualiser";
-import Visuals from "./Visuals";
 
-let x = 50;
-let y = 50;
 let poseNet;
 let poses = [];
 let video;
-let width = 500;
-let height = 500;
 
 function modelReady() {
   $('#status').html('Model Loaded');
 }
 
 export default function PoseNet() {
-
-  const [poseAnalysis, setPoseAnalysis] = useState({})
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -36,12 +28,6 @@ export default function PoseNet() {
     canvas.id("canvasOutput")
     canvas.position(0,0);
 
-    // p5.createCanvas(640, 480);
-    // video = p5.createCapture(p5.VIDEO);
-    // video.size(width, height);
-    // setVideo(video);
-
-
     video = document.getElementById('videoInput')
     // Create a new poseNet method with a single detection
     poseNet = ml5.poseNet(video, modelReady, {});
@@ -55,27 +41,14 @@ export default function PoseNet() {
   };
 
   const draw = (p5) => {
-    // let video = p5._elements[1]
-    // if (video === undefined) {
-    //   debugger;
-    // }
-
-
     p5.clear()
-    // p5.image(video, 0, 0, p5.width, p5.height);
 
     // We can call both functions to draw all keypoints and the skeletons
     drawKeypoints(p5, poses);
     drawSkeleton(p5, poses);
-
-    // NOTE: Do not use setState in the draw function or in functions that are executed
-    // in the draw function...
-    // please use normal variables or class properties for these purposes
-    x++;
   };
 
   return <>
-      {/*<Visuals poseAnalysis={poseAnalysis}/>*/}
       <p id="status">Loading model...</p>
       <Sketch setup={setup} draw={draw}/>
     </>;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,39 +1,7 @@
-import { useEffect } from 'react';
-import Player from '../utils/player';
-import Selector from '../utils/selector';
 import PoseNet from "../components/PoseNet";
 
 function App() {
-  const tracks = [
-    {filename: 'loops/130_Synth_Loop_15_134_SP_dvqp8a.wav'},
-    {filename: 'loops/130_Cm_Melodic_SPFT_126_05_k9f2zx.wav'}
-  ]
-  const options = {
-    bpm: 130,
-    // barLength: 4000,
-  }
-
-  const player = new Player(tracks, options)
-  const selector = new Selector(player)
-  window.selector = selector
-
-  return (
-    <div class="container m-auto">
-      <p class="text-center text-6xl font-bold mt-44"> BodyBeat </p>
-      <div class="m-auto">
-        <button class="bg-orange-200 p-2" onClick={() => player.startAll()}>START1</button>
-        <button class="bg-orange-200 p-2" onClick={() => selector.init()}>START2</button>
-        <button class="bg-orange-200 p-2" onClick={() => selector.process(2)}>ADDENERGY</button>
-        <button class="bg-orange-200 p-2" onClick={() => selector.process(0)}>REMOVEENERGY</button>
-        <PoseNet/>
-        {/* <button class="bg-orange-200 p-2" onClick={() => player.startOnBar(tracks[0].filename)}>QUEUE1</button>
-        <button class="bg-orange-200 p-2" onClick={() => player.startOnBar(tracks[1].filename)}>QUEUE2</button>
-        <button class="bg-orange-200 p-2" onClick={() => player.startAll()}>STARTALL</button>
-        <button class="bg-orange-200 p-2" onClick={() => player.stopAll()}>STOPALL</button>
-        <button class="bg-orange-200 p-2" onClick={() => player.setParamFunction(tracks[0].filename, 'gain', 0, 4)}>CHANGEGAIN1</button> */} */}
-      </div>
-    </div>
-  );
+  return <PoseNet/>;
 }
 
 export default App;
